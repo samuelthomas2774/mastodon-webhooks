c8a4f7b47737041b7558113f3e351802280f9c19
Samuel Elliott <samuel@fancy.org.uk>
2022-11-19 22:38:30 +0000

    Add unfollow command

--- src/discord.ts
+++ src/discord.ts
@@ -34,6 +34,15 @@ const commands = [
             .setRequired(true))
         .setDefaultMemberPermissions(PermissionFlagsBits.ManageWebhooks)
         .toJSON(),
+    new SlashCommandBuilder()
+        .setName('unfollow')
+        .setDescription('Unfollow an ActivityPub actor and stop sending their statuses in this channel')
+        .addStringOption(option => option
+            .setName('id')
+            .setDescription('username, account URI or local Mastodon account ID')
+            .setRequired(true))
+        .setDefaultMemberPermissions(PermissionFlagsBits.ManageWebhooks)
+        .toJSON(),
 ];
 
 export default class DiscordBot {
@@ -99,6 +108,11 @@ export default class DiscordBot {
                 if (interaction.customId.startsWith('follow:')) {
                     const id = interaction.customId.substr(7);
                     await this.handleConfirmFollowButton(interaction, id);
+                }
+
+                if (interaction.customId.startsWith('unfollow:')) {
+                    const id = interaction.customId.substr(9);
+                    await this.handleConfirmUnfollowButton(interaction, id);
                 }
             }
         } catch (err) {
@@ -343,6 +357,133 @@ export default class DiscordBot {
             webhooks: webhooks.filter(w => filter_accts.find(f => f.webhook_id === w.id)),
             filter_accts,
         };
+    }
+
+    async handleUnfollowCommand(interaction: ChatInputCommandInteraction, id: string) {
+        const channel = interaction.channel?.isThread() ? interaction.channel.parent : interaction.channel;
+        const thread = interaction.channel?.isThread() ? interaction.channel : undefined;
+
+        if (!interaction.guild || !channel) {
+            await interaction.reply('This action can only be performed in text channels in servers.');
+            return;
+        }
+
+        await interaction.deferReply();
+
+        const accounts = await this.findAccounts(id);
+
+        if (accounts.length > 1) {
+            await interaction.editReply('Multiple results found. Look up a user with the `/lookup` command and try again using a URL, or use the `/following` command to check which users are followed in this channel.');
+            return;
+        }
+        if (!accounts.length) {
+            await interaction.editReply('No results found.');
+            return;
+        }
+
+        const account = accounts[0];
+
+        const acct = account.acct.includes('@') ?
+            account.acct : account.acct + '@' + this.mastodon.account_host;
+
+        if (account.id !== id &&
+            acct !== id &&
+            '@' + acct !== id &&
+            account.url !== id
+        ) {
+            const embed = this.createAccountEmbed(account);
+
+            // Check if a webhook exists
+            const {filter_accts} = await this.getWebhooksForChannelActor(interaction.guild, channel, thread, acct);
+
+            if (!filter_accts.length) {
+                await interaction.editReply({
+                    content: 'This actor is not being following in this channel.\n\n' + account.url,
+                    embeds: [embed],
+                });
+                return;
+            }
+
+            // Ask for confirmation
+            const row = new ActionRowBuilder()
+                .addComponents(new ButtonBuilder()
+                    .setCustomId('unfollow:' + account.id)
+                    .setLabel('Confirm')
+                    .setStyle(ButtonStyle.Primary));
+
+            await interaction.editReply({
+                content: 'Unfollow this user?\n\n' + account.url,
+                embeds: [embed],
+                components: [row as any],
+            });
+            return;
+        }
+
+        // Select this user
+        await this.unfollow(interaction, interaction.guild, channel, thread, account);
+    }
+
+    async handleConfirmUnfollowButton(interaction: ButtonInteraction, id: string) {
+        const channel = interaction.channel?.isThread() ? interaction.channel.parent : interaction.channel;
+        const thread = interaction.channel?.isThread() ? interaction.channel : undefined;
+
+        if (!interaction.guild || !channel) {
+            await interaction.reply('This action can only be performed in text channels in servers.');
+            return;
+        }
+
+        await interaction.deferReply();
+
+        const account: Account = await this.mastodon.fetch('/api/v1/accounts/' + id);
+
+        await this.unfollow(interaction, interaction.guild, channel, thread, account);
+    }
+
+    async unfollow(
+        interaction: ChatInputCommandInteraction | ButtonInteraction,
+        guild: Guild, channel: TextBasedChannel | ForumChannel,
+        thread: PrivateThreadChannel | PublicThreadChannel | undefined,
+        account: Account
+    ) {
+        const acct = account.acct.includes('@') ?
+            account.acct : account.acct + '@' + this.mastodon.account_host;
+
+        // Check if a webhook already exists
+        const {webhooks, filter_accts} = await this.getWebhooksForChannelActor(guild, channel, thread, acct);
+
+        if (!filter_accts.length) {
+            await interaction.editReply('This actor is not being following in this channel.');
+            return;
+        }
+
+        for (const filter_acct of filter_accts) {
+            const result = await this.db.run(sql`DELETE FROM webhook_filter_acct WHERE id = ${filter_acct.id}`);
+
+            debug('Deleted webhook account filter', filter_acct, result);
+        }
+
+        for (const webhook of webhooks) {
+            const filter_accts = await this.db.get<{count: number}>(
+                sql`SELECT COUNT(*) as count FROM webhook_filter_acct WHERE webhook_id = ${webhook.id}`);
+            if (filter_accts?.count) continue;
+
+            const filter_hosts = await this.db.get<{count: number}>(
+                sql`SELECT COUNT(*) as count FROM webhook_filter_host WHERE webhook_id = ${webhook.id}`);
+            if (filter_hosts?.count) continue;
+
+            const result = await this.db.run(sql`DELETE FROM webhooks WHERE id = ${webhook.id}`);
+
+            debug('Deleted webhook', webhook, result);
+        }
+
+        const embed = this.createAccountEmbed(account);
+
+        const message = {
+            content: 'Unfollowed ' + account.url,
+            embeds: [embed],
+        };
+
+        await interaction.editReply(message);
     }
 
     async findAccounts(q: string) {
